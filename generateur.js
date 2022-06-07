59cb000892f556dffaf88a79d7402ff2acf9374f
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-07 16:41:57 +0200

    Générateur de mot de passe de 8 caractères de long.

--- generateur.js
+++ generateur.js
@@ -1,1 +1,23 @@
-console.log("test");
+const characters = ["a", "b", "c", "d", "E", "F", "G", "H", 1, 2, 3, 4, 5, 6, 7, 8];
+const random_element = [Math.floor(Math.random() * characters.length)];
+
+function get_random_int(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const random_number = get_random_int(10);
+
+function generate_password(){
+    for (let i = 0; i < 8; i++) {
+        if(characters[i]){
+            while (i != 8) {
+                console.log(`Votre mot de passe contient le caractère suivant : " ${characters[get_random_int(16)]} "`);
+                break;
+            }
+        }else{
+            console.log("error");
+        }
+    }
+    console.log("bien joué");
+}
+generate_password();
