acf865d1c9c7038955f9f5d956dd83b8433fda67
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-07 16:45:25 +0200

    Optimisations.

--- generateur.js
+++ generateur.js
@@ -1,11 +1,9 @@
 const characters = ["a", "b", "c", "d", "E", "F", "G", "H", 1, 2, 3, 4, 5, 6, 7, 8];
-const random_element = [Math.floor(Math.random() * characters.length)];
+// const random_element = [Math.floor(Math.random() * characters.length)];
 
 function get_random_int(max) {
     return Math.floor(Math.random() * max);
 }
-
-const random_number = get_random_int(10);
 
 function generate_password(){
     for (let i = 0; i < 8; i++) {
