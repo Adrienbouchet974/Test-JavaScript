99f00d6836bea78d3f4497b2d2e919e4ba3be4ca
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-07 15:01:58 +0200

    Ajout de code.

--- numéro_mystère.js
+++ numéro_mystère.js
@@ -1,19 +1,42 @@
+const config = {
+  noob: {
+    attemp: 5,
+    mysteryNumber: [1, 10],
+  },
+  beginner: {
+    attemp: 4,
+    mysteryNumber: [1, 25],
+  },
+  hard: {
+    attemp: 2,
+    mysteryNumber: [1, 50],
+  },
+  hell: {
+    attemp: 1,
+    mysteryNumber: [1, 100],
+  },
+};
+
 function get_random_int(max) {
   return Math.floor(Math.random() * max);
 }
 
 const nombre = [1, 2, 3, 4];
+attemp = 5;
 
 const random_number = get_random_int(10);
 console.log(random_number);
 
-
-for (let i = 0; i < 3; i++) {
-  console.log(nombre[i]);
-  if(random_number == nombre[i]){
-    console.log(`bien joué trouvé en ${i}`);
-  }else{
-    console.log("pas de chance");
+function launch(nombre, config) {
+  for (let i = 0; i < 3; i++) {
+    console.log(nombre[i]);
+    if(random_number == nombre[i]){
+      console.log(`bien joué trouvé en ${i}`);
+    }else{
+      console.log("pas de chance");
+    }
   }
+  console.log(`le nombre était ${random_number}`);
 }
-console.log(`le nombre était ${random_number}`);
+
+launch([2, 3, 5, 6, 7],config.noob.attemp);
